bf590d0695238b39e4fde0dcc51ee140cb9916f1
Aquino Nyapara <aquino.nyapara@gmail.com>
2024-08-27 04:01:07 +0300

    Update block.py

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from .conv import Conv, DWConv, GhostConv, LightConv, RepConv, autopad
+from .conv import Conv, SimConv, DWConv, GhostConv, LightConv, RepConv, autopad
 from .transformer import TransformerBlock
 from ultralytics.utils.torch_utils import fuse_conv_and_bn
 
@@ -15,6 +15,7 @@ __all__ = (
     "HGStem",
     "SPP",
     "SPPF",
+    "SimSPPF",
     "C1",
     "C2",
     "C3",
@@ -176,7 +177,22 @@ class SPPF(nn.Module):
         y1 = self.m(x)
         y2 = self.m(y1)
         return self.cv2(torch.cat((x, y1, y2, self.m(y2)), 1))
-
+        
+class SimSPPF(nn.Module):
+    '''Simplified SPPF with ReLU VAN_activation'''
+    
+    def __init__(self, c1, c2, k=5):
+        super().__init__()
+        c_ = c1 // 2  # hidden channels
+        self.cv1 = SimConv(c1, c_, 1, 1)
+        self.cv2 = SimConv(c_ * 4, c2, 1, 1)
+        self.m = nn.MaxPool2d(kernel_size=k, stride=1, padding=k // 2)
+    
+    def forward(self, x):
+        x = self.cv1(x)
+        y1 = self.m(x)
+        y2 = self.m(y1)
+        return self.cv2(torch.cat([x, y1, y2, self.m(y2)], 1))
 
 class C1(nn.Module):
     """CSP Bottleneck with 1 convolution."""
